71804a43f156a3de8d52e523fa91c2b3be7fe97f
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-11-12 14:08:34 -0500

    Working Article Search with orentation change enabled and default home page

---
+++ Lab/Lab6 ArticleSearchPt-3/app/src/main/java/com/codepath/articlesearch/BlankFragment.kt
@@ -0,0 +1,8 @@
+package com.codepath.articlesearch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM3 = "param3"
+
